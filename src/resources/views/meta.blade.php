0138fd5d2f4370d20eba30cd4cd1614d6b86f6c9
Chris <chris_J_Kelker@hotmail.com>
2023-03-29 18:39:14 +0100

    Tweaks: removed some unnecessary favicon meta data

--- src/resources/views/meta.blade.php
+++ src/resources/views/meta.blade.php
@@ -1,4 +1,4 @@
-@env(config()->get('favicon.environments'))
+@env(config('favicon.environments'))
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
   <link rel="icon" type="image/png" sizes="384x384" href="/android-chrome-384x384.png">
@@ -7,7 +7,4 @@
   <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png">
   <link rel="manifest" href="/site.webmanifest">
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
 @endenv
-
